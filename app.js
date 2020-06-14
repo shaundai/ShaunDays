290862d28c45087fa5ab179a751f53a14516276b
Shaundai Person <shaundaiperson@gmail.com>
2020-06-14 17:04:50 -0400

    attempt to get it to work

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-import axios from 'axios';
+const axios = require('axios')
+const getWeather = require('./weatherApi')
 
-const http = require('http');
-const axios = require('axios').default;
-
-// Create an instance of the http server to handle HTTP requests
-let app = http.createServer((req, res) => {
-    // Set a response type of plain text for the response
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // Send back a response and end the connection
-    res.end('Hello World!\n');
-});
-
-// Start the server on port 3000
-app.listen(3000, '127.0.0.1');
-console.log('Node server running on port 3000');
-
-
-
-const getWeather = async () => {
-    try {
-        const response = await axios.get('https://api.darksky.net/forecast/678da8f7dbcf7a5d50a3533a310238d1/33.9526,84.5499')
-        .then(console.log(`SUCCESS: ${response}`))
-    } catch (err) {
-        console.log(err);
-    }
-}
+console.log(axios)
+console.log(getWeather())
