ad1aeba7375dff18ef54b5cb4cf6b26eb77557ea
Shaundai Person <shaundaiperson@gmail.com>
2020-06-14 18:04:45 -0400

    change the dom

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const axios = require('axios')
-const getWeather = require('./weatherApi')
+document.getElementById("header").innerHTML = 'this is a Weather App'
 
-console.log(axios)
-console.log(getWeather())
+
+const axios = require('axios');
+
+const getWeather = async () => {
+    const response = await axios({
+        url: 'https://api.darksky.net/forecast/678da8f7dbcf7a5d50a3533a310238d1/33.9526,-84.5499'
+    })
+    console.log(response.data.daily.data);
+}
+
+getWeather()
+
