1b74befc873c4cd6dd286b7657de5a529c548072
Shaundai Person <shaundaiperson@gmail.com>
2020-06-14 00:02:28 -0400

    learn npm

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+import axios from 'axios';
+
 const http = require('http');
+const axios = require('axios').default;
 
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
@@ -12,3 +15,14 @@ let app = http.createServer((req, res) => {
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
+
+
+
+const getWeather = async () => {
+    try {
+        const response = await axios.get('https://api.darksky.net/forecast/678da8f7dbcf7a5d50a3533a310238d1/33.9526,84.5499')
+        .then(console.log(`SUCCESS: ${response}`))
+    } catch (err) {
+        console.log(err);
+    }
+}
