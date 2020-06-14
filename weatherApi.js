3ce11c936f91bc9226231206b54250bae856ee24
Shaundai Person <shaundaiperson@gmail.com>
2020-06-13 22:54:25 -0400

    add npm

---
+++ weatherApi.js
@@ -0,0 +1,10 @@
+const axios = require('axios').default;
+
+const getWeather = async () => {
+    try {
+        const response = await axios.get('https://api.darksky.net/forecast/678da8f7dbcf7a5d50a3533a310238d1/33.9526,84.5499')
+        .then(console.log(`SUCCESS: ${response}`))
+    } catch (err) {
+        console.log(err);
+    }
+}
