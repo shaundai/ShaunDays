8bcbbc5ce83bb7e6d4395e1d06171d45604df8ed
Shaundai Person <shaundaiperson@gmail.com>
2020-06-14 23:38:57 -0400

    test react

--- weatherApi.js
+++ weatherApi.js
@@ -1,4 +1,7 @@
-getWeather = async () => {
+const axios = require('axios');
+
+
+const getWeather = async () => {
     try {
         const response = await axios.get('https://api.darksky.net/forecast/678da8f7dbcf7a5d50a3533a310238d1/33.9526,84.5499')
         .then(console.log(`SUCCESS: ${response}`))
