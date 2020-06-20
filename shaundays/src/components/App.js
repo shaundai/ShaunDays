bd1c03d0e7a7376968e2708d6a184c747e8f057e
Shaundai Person <shaundaiperson@gmail.com>
2020-06-20 18:31:03 -0400

    start five day weather component

--- shaundays/src/components/App.js
+++ shaundays/src/components/App.js
@@ -8,51 +8,53 @@ require('dotenv').config()
 
 export default function App() {
   const [mariettaWeather, setMariettaWeather] = useState({})
+  const [fiveDayForecast, setFiveDayForecast] = useState({})
+  const [weatherButtonText, setWeatherButtonText] = useState('Get Weather')
   const [time, setTime] = useState('')
 
   const getSummary = async () => {
     try {
     const weather = (await getWeather()).data.currently
     setMariettaWeather(weather);
+    setWeatherButtonText('5-Day Forecast');
     }
     catch(err){
       console.log(`My error code is ${err.status}.  I errored out bc ${err}`)
     }
   }
 
-  const getTime = (time) => {
-    const newTime = convertTimestampToTime(mariettaWeather.time);
-    return newTime;
+  const getFiveDay = async () => {
+    try {
+    const fiveDayWeather = (await getWeather()).data.daily
+    setFiveDayForecast(fiveDayWeather);
+    setWeatherButtonText('Get Todays Weather');
+    }
+    catch(err){
+      console.log(`My error code is ${err.status}.  I errored out bc ${err}`)
+    }
   }
-
-
-
 
   return (
     <div className="App">
-        <Shaun>Shaun <Days>Days</Days></Shaun>
-        <h1>Weather App</h1>
-        <h2>Marietta Weather</h2> 
-        <h1>{mariettaWeather.summary}</h1>
-        <img src={mariettaWeather.icon ? require(`../images/${mariettaWeather.icon}.png`) : require(`../images/default.png`)}></img>
-        <button onClick={getSummary}>Get Today's Weather</button>
+        <h1 style={{fontSize: '2.5em', margin: 0, paddingBottom: 0}}><Shaun>Shaun</Shaun><Days>Days</Days></h1>
+        <div style={{fontSize: '2em', marginTop: 0, marginBottom: '.5em', paddingTop: 0, color: '#317873'}}>Weather App</div>
+        <div style={{backgroundColor: 'gray', borderRadius: 20, padding: 15, alignItems: 'center', justifyContent: 'center'}}>
+          <h2 style={{fontSize: '1.5em', marginTop: 10, paddingTop: 0}}>Marietta, GA</h2> 
+          <div>{mariettaWeather.summary}</div>
+          {mariettaWeather.icon ? <img className="mainWeatherIcon" src={require(`../images/${mariettaWeather.icon}.png`)}></img> : null}
+          {mariettaWeather.temperature ? <div>{mariettaWeather.temperature} F</div> : null}
+          <button onClick={getSummary}>{weatherButtonText}</button>
+        </div>
     </div>
   );
 }
 
-const Shaun = styled.h1`
-  font-size: 2em;
+const Shaun = styled.span`
   text-align: left;
-  color: #317873;
-
 `;
 
 const Days = styled.span`
   text-align: center;
-  font-family: Trebuchet MS, Helvetica, sans-serif;
-  color: #4FBCC5;
-`
-
-const mainWeatherIcon = styled.img`
-  height: 40em;
+  text-decoration: underline;
+  color: #317873;
 `
---
+++ shaundays/src/components/FiveDay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function FiveDay() {
+    
+    return (
+            <div>
+                
+            </div>
+        )
+    }
