7c2bca7269852454fb0157b8a5264397bb9a2618
Shaundai Person <shaundaiperson@gmail.com>
2020-06-21 01:26:22 -0400

    start to add five day forecast

--- shaundays/src/components/App.js
+++ shaundays/src/components/App.js
@@ -2,21 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import styled from 'styled-components';
 import getWeather from './util/weatherApi';
-import convertTimestampToTime from './util/utilities';
+import FiveDay from './FiveDay';
 require('dotenv').config()
 
 
 export default function App() {
   const [mariettaWeather, setMariettaWeather] = useState({})
+  const [getFiveDayOn, setGetFiveDayOn] = useState(false)
   const [fiveDayForecast, setFiveDayForecast] = useState({})
-  const [weatherButtonText, setWeatherButtonText] = useState('Get Weather')
-  const [time, setTime] = useState('')
 
   const getSummary = async () => {
     try {
     const weather = (await getWeather()).data.currently
     setMariettaWeather(weather);
-    setWeatherButtonText('5-Day Forecast');
+    setGetFiveDayOn(true)
     }
     catch(err){
       console.log(`My error code is ${err.status}.  I errored out bc ${err}`)
@@ -25,36 +24,60 @@ export default function App() {
 
   const getFiveDay = async () => {
     try {
-    const fiveDayWeather = (await getWeather()).data.daily
+    const fiveDayWeather = (await getWeather()).data.daily.data
     setFiveDayForecast(fiveDayWeather);
-    setWeatherButtonText('Get Todays Weather');
     }
     catch(err){
       console.log(`My error code is ${err.status}.  I errored out bc ${err}`)
     }
   }
 
+  const showFiveDay = (forecast) => {
+    const fiveDay = fiveDayForecast.map(item => {
+      return <FiveDay summary={item.summary} icon={item.icon} temperature={item.temperatureHigh} />
+    });
+    return fiveDay;
+  }
+
   return (
     <div className="App">
-        <h1 style={{fontSize: '2.5em', margin: 0, paddingBottom: 0}}><Shaun>Shaun</Shaun><Days>Days</Days></h1>
-        <div style={{fontSize: '2em', marginTop: 0, marginBottom: '.5em', paddingTop: 0, color: '#317873'}}>Weather App</div>
+        <div style={{zIndex: 10, margin: 0, paddingBottom: 0}}>
+          <h1 style={{fontSize: '2.5rem', margin: 0, paddingBottom: 0}}>
+          <Shaun>Shaun</Shaun><Days>Days</Days>
+        </h1>
+        </div>
+        <Grayrectangle/>
+
+        <div style={{fontSize: '2rem', marginTop: -2, marginBottom: '1em', paddingTop: 0, color: '#317873'}}>Weather App</div>
         <div style={{backgroundColor: 'gray', borderRadius: 20, padding: 15, alignItems: 'center', justifyContent: 'center'}}>
-          <h2 style={{fontSize: '1.5em', marginTop: 10, paddingTop: 0}}>Marietta, GA</h2> 
+          <h2 style={{fontSize: '1.5rem', marginTop: 10, paddingTop: 0}}>Marietta, GA</h2> 
           <div>{mariettaWeather.summary}</div>
           {mariettaWeather.icon ? <img className="mainWeatherIcon" src={require(`../images/${mariettaWeather.icon}.png`)}></img> : null}
           {mariettaWeather.temperature ? <div>{mariettaWeather.temperature} F</div> : null}
-          <button onClick={getSummary}>{weatherButtonText}</button>
+          {getFiveDayOn ? <button onClick={getFiveDay}>Get Five Day Forecast</button> : <button onClick={getSummary}>Get Today's Weather</button>}
         </div>
+        {getFiveDayOn ? <FiveDay weather={mariettaWeather} /> : null}
     </div>
   );
 }
 
 const Shaun = styled.span`
   text-align: left;
+  z-index: -1;
 `;
 
 const Days = styled.span`
   text-align: center;
   text-decoration: underline;
   color: #317873;
+`;
+
+const Grayrectangle = styled.div`
+  background-color: #F5F5F5;
+  height: 1rem;
+  width: 12rem;
+  position: relative;
+  bottom: .8rem;
+  opacity: 0.5;
+  margin-bottom: -2;
 `
--- shaundays/src/components/FiveDay.js
+++ shaundays/src/components/FiveDay.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-function FiveDay() {
-    
+function FiveDay({summary, icon, temperatureHigh}) {
+
     return (
-            <div>
-                
-            </div>
+        <div style={{backgroundColor: 'gray', borderRadius: 20, padding: 15, alignItems: 'center', justifyContent: 'center'}}>
+          <h2 style={{fontSize: '1.5rem', marginTop: 10, paddingTop: 0}}>Marietta, GA</h2> 
+          <div>{summary}</div>
+          {icon ? <img className="mainWeatherIcon" src={require(`../images/${icon}.png`)}></img> : null}
+          {temperatureHigh ? <div>High: {temperatureHigh} F</div> : null}
+        </div>
         )
     }
+
+export default FiveDay;
