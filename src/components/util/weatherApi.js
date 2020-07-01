31575b0b7b38b75753b73523fb86549d0a0bbb10
Shaundai Person <shaundaiperson@gmail.com>
2020-07-01 17:34:14 -0400

    update to call from proxy

--- src/components/util/weatherApi.js
+++ src/components/util/weatherApi.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const axios = require('axios');
 
 const getWeather = (lat, long) => {
-        return axios.get(`http://wednesday-tesla.herokuapp.com/api/weather/${lat},${long}`)
+        return axios.get(`http://localhost:4000/weather/${lat},${long}`)
 }
 
 export default getWeather;
