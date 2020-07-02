d0e716646ce55cd37827d8f53bdec5ae3b1a88b9
Shaundai Person <shaundaiperson@gmail.com>
2020-07-01 23:52:14 -0400

    small update to api

--- src/components/util/weatherApi.js
+++ src/components/util/weatherApi.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const axios = require('axios');
 
 const getWeather = (lat, long) => {
-        return axios.get(`http://localhost:4000/weather/${lat},${long}`)
+        return axios.get(`http://shaundays-node.herokuapp.com/weather/${lat},${long}`)
 }
 
 export default getWeather;
