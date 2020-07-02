2b9225b393427d0c4a64d231d0e64f161c7ccedc
Shaundai Person <shaundaiperson@gmail.com>
2020-07-02 11:51:05 -0400

    add cors anywhere

--- src/components/util/weatherApi.js
+++ src/components/util/weatherApi.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const axios = require('axios');
 
 const getWeather = (lat, long) => {
-        return axios.get(`http://shaundays-node.herokuapp.com/weather/${lat},${long}`)
+        return axios.get(`https://cors-anywhere.herokuapp.com/http://shaundays-node.herokuapp.com/weather/${lat},${long}`)
 }
 
 export default getWeather;
